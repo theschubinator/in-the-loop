4557c9a6d1ac6d1ec9d7ad075f0b04ffda05cb34
github username <github email address>
2017-05-24 09:10:56 -0500

    created admin, user roles to users model. Defaults to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         has_many :tasks
-         has_many :categories, through: :tasks #not working
+
+  has_many :tasks
+  has_many :categories, through: :tasks
+
+  enum role: [:user, :admin]
+
+  after_initialize :set_default_role
+
+  def set_default_role
+  	self.role || self.role = :user
+  end
+  
 end
