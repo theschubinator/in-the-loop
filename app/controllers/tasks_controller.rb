24ab065302fdcd5eddc0809cd38057d7c213e856
github username <github email address>
2017-05-24 10:01:37 -0500

    created task#new, and task#index. User can now create a new task.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,8 +4,20 @@ class TasksController < ApplicationController
 	end
 
 	def new
+		@task = Task.new
+	end
+
+	def create
+		current_user.tasks.build(task_params)
+		current_user.save
+		redirect_to tasks_path
 	end
 
 	def show
 	end
+
+	private
+	  def task_params
+	  	params.require(:task).permit(:title, :description)
+	  end
 end
