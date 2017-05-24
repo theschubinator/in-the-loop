64b8e3f293717ec9c8a545e32c182ac44a6e8ef8
github username <github email address>
2017-05-24 15:21:51 -0500

    added task#edit, and task#update so users can update tasks. Views added as well'

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,25 +1,38 @@
 class TasksController < ApplicationController
 	def index
-		@tasks = current_user.tasks
+	  @tasks = current_user.tasks
 	end
 
 	def new
-		@task = Task.new
+	  @task = Task.new
 	end
 
 	def create
-		binding.pry
-		current_user.tasks.build(task_params)
-		current_user.save
-		redirect_to tasks_path
+	  current_user.tasks.build(task_params)
+	  current_user.save
+	  redirect_to tasks_path
+	end
+
+	def edit
+	  find_task
+	end
+
+	def update
+	  find_task
+	  @task.update(task_params)
+	  redirect_to task_path(@task)
 	end
 
 	def show
-		@task = Task.find(params[:id])
+	  find_task
 	end
 
 	private
 	  def task_params
 	  	params.require(:task).permit(:title, :description, category_ids: [])
+	  end
+
+	  def find_task
+	  	@task = Task.find(params[:id])
 	  end
 end
