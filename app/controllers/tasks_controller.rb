97f4ac07bf2d28c5e6928ac3cdaba3758e1a3089
github username <github email address>
2017-05-25 12:30:24 -0500

    categories are now added to tasks via task_params and not its own strong parameters"

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,11 +8,8 @@ class TasksController < ApplicationController
 	end
 
 	def create
-		@user = current_user
 	  current_user.tasks.build(task_params)
-	  current_user.tasks.last.categories << Category.create(category_params) if !category_params[:name].empty?
 	  current_user.save
-	#  redirect_to tasks_path
 	  redirect_to user_tasks_path(current_user)
 	end
 
@@ -23,7 +20,6 @@ class TasksController < ApplicationController
 	def update
 	  find_task
 	  @task.update(task_params)
-	  @task.categories << Category.create(category_params) if !category_params[:name].empty?
 	  redirect_to user_task_path(current_user, @task)
 	end
 
@@ -40,11 +36,7 @@ class TasksController < ApplicationController
 
 	private
 	  def task_params
-	  	params.require(:task).permit(:title, :description, category_ids: [])
-	  end
-
-	  def category_params
-	  	params.require(:categories).permit(:name)
+	  	params.require(:task).permit(:title, :description, category_ids: [], categories_attributes: [:name])
 	  end
 
 	  def find_task
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
 	belongs_to :user
 	has_many :task_categories
 	has_many :categories, through: :task_categories
+	accepts_nested_attributes_for :categories
 end
